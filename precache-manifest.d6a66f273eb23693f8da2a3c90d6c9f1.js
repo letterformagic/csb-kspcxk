be5ccefa810b4398981adfbd3cf9544727c42d88
Code Sandbox <hello@codesandbox.io>
2022-10-14 20:16:52 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d6a66f273eb23693f8da2a3c90d6c9f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b1246c15882656f2faecb50ea7e105",
+    "url": "https://tihanasare.github.io/csb-kspcxk/index.html"
+  },
+  {
+    "revision": "e5824439cfa67a280a0c",
+    "url": "https://tihanasare.github.io/csb-kspcxk/static/css/main.5a17b406.chunk.css"
+  },
+  {
+    "revision": "daccdacfd2eaee88f057",
+    "url": "https://tihanasare.github.io/csb-kspcxk/static/js/2.d837cda9.chunk.js"
+  },
+  {
+    "revision": "e5824439cfa67a280a0c",
+    "url": "https://tihanasare.github.io/csb-kspcxk/static/js/main.9ce839db.chunk.js"
+  },
+  {
+    "revision": "05a07ba017c0e302458f",
+    "url": "https://tihanasare.github.io/csb-kspcxk/static/js/runtime~main.ae4482a6.js"
+  }
+]);
